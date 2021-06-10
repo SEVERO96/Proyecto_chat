db3701a8a4b80763f451e8feed630a13f22b15ca
Severo_iglesias <siglesias@ucol.mx>
2021-06-10 09:32:00 -0500

    practica 10

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,24 @@
 const chatForm = document.getElementById('chat-form')
+// Constante que toma el estilo del elemento de mi document
 const chatMessage = document.querySelector('.chat-messages')
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+// Obteniendo username y room del URL
+const {username,room} = Qs.parse(location.search,{
+  ignoreQueryPrefix: true
+});
 
 const socket = io();
+
+//Unirse a la sala
+socket.emit('joinRoom', {username, room});
 
 socket.on('message', message =>{
   //console.log(message);
   outputMessage(message);
 
+  //Top del scroll
   chatMessage.scrollTop = chatMessage.scrollHeight;
-
 });
 
 chatForm.addEventListener('submit',(e)=>{
@@ -19,9 +29,10 @@ chatForm.addEventListener('submit',(e)=>{
 
   socket.emit('chatMessage', msg)
 
-    //limpiar input y mantener el focus en el input
-    e.target.elements.msg.value ='';
-    e.target.elements.msg.focus();
+  //limpiar input y mantener el focus en el input
+  e.target.elements.msg.value ='';
+  e.target.elements.msg.focus();
+
   //console.log(msg)
 });
 
